f7d31a33c5521c3d4035969d810b9cc937344b24
i0nsane <k.quambusch@cfr0gz.de>
2011-10-31 19:54:10 +0100

    Added some comments ;)

--- srcCrypto/SymCrypt.py
+++ srcCrypto/SymCrypt.py
@@ -29,21 +29,42 @@ class SymCrypt:
 				AES.MODE_CBC).decrypt(ciphertext)
 		return self.removePadding(ciphertext)
 
-    def encryptCompressed(self, filename, ciphertext, passphrase, persist=True):
-        ciphertext = bz2.compress(ciphertext)
+    def encryptCompressed(self, filename=None, ciphertext, passphrase, persist=True):
+        try:
+            ciphertext = bz2.compress(ciphertext)
+
+        except Exception as msg:
+            print msg
+            res = input(r"> ")
+            print res
+
         ciphertext = ciphertext.encode("hex")
-        fileID = str(uuid4())
+        
+        if filename is None:
+            fileID = str(uuid4())
+        else:
+            fileID = filename
+        
         padding = ""
+        # Wrap our data into a headerfile in JSON format (Python Dictionary)
         cipherheader = {'f': fileID,
                         's': len(ciphertext),
                         'd': ciphertext
                         }
+
+        # the cipherheader is dumped to a string by the JSON module
         return self.encryptAES(dumps(cipherheader), passphrase)
         
     def decryptCompressed(self, input=None, passphrase=None, persist=True):
+        
+        # Look for data and serialize decrypted object
         res = loads(self.decryptAES(input, passphrase))
+        # Deflate data
         res['d'] = bz2.decompress(res['d'].decode("hex"))
+        
+        # if the persist parameter was set to true (default)
         if persist is True:
+            # we want to save the object on harddisk so go on:
             fobj = open("cmnObjectCache/%s" % (res['f']), 'wb')
             dump(res, fobj)
             fobj.flush()
