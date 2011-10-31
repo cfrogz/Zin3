3b1e3db5f68dd3105b9dedd225dd0c52cd92aa22
i0nsane <k.quambusch@cfr0gz.de>
2011-10-31 19:47:39 +0100

    Added Support for Encrypted compressed containers Will integrate Header for Data in every function.

--- srcTracker/silence.py
+++ srcTracker/silence.py
@@ -15,30 +15,40 @@ Danach wird mit \x00 aufgefuellt bis der container durch 16
 teilbar ist.
 '''
 
+class common(object):
+    def recvData(self, addr, filesize):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            port = random.randint(49152, 65535)
+            print port
+            sock.bind(("", int(port)))
+            sock.connect((addr, int(port)))
+            data = sock.recv(filesize)
+    
+        except Exception as msg:
+            print msg
+            common().recvData(addr, filesize)
+
+        return data
+
+
 class communicator(SocketServer.BaseRequestHandler):
-    def call(self):
-        HOST, PORT = "localhost", 9764
-        server = SocketServer.TCPServer((HOST, PORT), communicator)
-        server.serve_forever()
 # write this crap with select on socket
     def handle(self):
         # struct which inits: "!6sI" == "IIINIT", unsigned int [Filesize]
         inputRequest, addr = self.request.recvfrom(10)
         init, filesize = struct.unpack("!6sI", inputRequest)
-        data = communicator.recvData(addr, filesize)
+        print init, filesize
+        data = common().recvData(addr, filesize)
+        print "done"
+        return 0
         # TODO: Encrypt data
         # TODO: Serialize data and pipe it to display or message etc
         
-    def recvData(self, addr, filesize):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            port = random.randint(49152, 65535)
-            sock.bind("", port)
-            sock.connect((addr, port))
-            data = sock.recv(filesize)
-    
-        except Exception as msg:
-            print msg
-            communicator.createSocket()
 
-        return data
+
+class scream(object):
+    def call(self):
+        HOST, PORT = "localhost", 9616
+        server = SocketServer.TCPServer((HOST, PORT), communicator)
+        server.serve_forever()
